ea7ee6c5021d08030aea5cf84bbe91f396feedf2
Saurav Kumar <shah.kumar.saurav@gmail.com>
2025-10-24 00:04:54 +0530

    added default advisor and loggings of the advisor to trace the request and response of the llm

--- Section_2/springai/src/main/java/com/tipsontech/springai/ChatClientConfig.java
+++ Section_2/springai/src/main/java/com/tipsontech/springai/ChatClientConfig.java
@@ -1,6 +1,7 @@
 package com.tipsontech.springai;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,6 +11,7 @@ public class ChatClientConfig {
     @Bean
     public ChatClient chatClient(ChatClient.Builder chatClientBuilder) {
         return chatClientBuilder
+                .defaultAdvisors(new SimpleLoggerAdvisor())
                 .defaultSystem("""
                        You are an internal HR assistant. Your role is to help\s
                        employees with questions related to HR policies, such as\s
