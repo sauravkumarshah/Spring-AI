50c02efa6fd69b17291a649b88e7f5f34aecf95b
Saurav Kumar <shah.kumar.saurav@gmail.com>
2025-10-23 17:58:20 +0530

    multiple chat models

---
+++ Section_1/multimodel/src/test/java/com/tipsontech/multimodel/OpenaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tipsontech.multimodel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
