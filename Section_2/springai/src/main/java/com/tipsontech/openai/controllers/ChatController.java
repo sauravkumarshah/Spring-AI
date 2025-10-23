808ca5611566efade6f9a4217e58f423efb08001
Saurav Kumar <shah.kumar.saurav@gmail.com>
2025-10-23 18:22:01 +0530

    using user and system roles

---
+++ Section_2/springai/src/main/java/com/tipsontech/openai/controllers/ChatController.java
@@ -0,0 +1,35 @@
+package com.tipsontech.openai.controllers;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class ChatController {
+
+    private final ChatClient  chatClient;
+
+    public ChatController(ChatClient.Builder chatClientBuilder) {
+        this.chatClient = chatClientBuilder.build();
+    }
+
+    @GetMapping("/chat")
+    public String chat(@RequestParam("message") String message) {
+       return chatClient
+               .prompt()
+               .system("""
+                       You are an internal HR assistant. Your role is to help\s
+                       employees with questions related to HR policies, such as\s
+                       leave policies, working hours, benefits, and code of conduct.
+                       If a user asks for help with anything outside of these topics,\s
+                       kindly inform them that you can only assist with queries related to\s
+                       HR policies.
+                       """)
+               .user(message)
+               .call()
+               .content();
+    }
+}
