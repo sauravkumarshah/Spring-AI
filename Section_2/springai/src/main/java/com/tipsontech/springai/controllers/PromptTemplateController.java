ed33885d6fce42bdd8aebe7396c3c57614383bf9
Saurav Kumar <shah.kumar.saurav@gmail.com>
2025-10-23 23:37:31 +0530

    moved user prompt template to the resource location

--- Section_2/springai/src/main/java/com/tipsontech/springai/controllers/PromptTemplateController.java
+++ Section_2/springai/src/main/java/com/tipsontech/springai/controllers/PromptTemplateController.java
@@ -1,6 +1,8 @@
 package com.tipsontech.springai.controllers;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -16,16 +18,8 @@ public class PromptTemplateController {
         this.chatClient = chatClient;
     }
 
-    String promptTemplate = """
-            A customer named {customerName} sent the following message:
-                           "{customerMessage}"
-                        
-            Write a polite and helpful email response addressing the issue.
-            Maintain a professional tone and provide reassurance.
-                        
-            Respond as if you're writing the email body only. Don't include subject,
-            signature
-            """;
+    @Value("classpath:/promptTemplates/userPromptTemplate.st")
+    Resource userPromptTemplate;
 
     @GetMapping("/email")
     public String emailResponse(@RequestParam("customerName") String customerName,
@@ -37,7 +31,7 @@ public class PromptTemplateController {
                         responses to improve the productivity of the customer support team
                         """)
                .user(promptTemplateSpec ->
-                       promptTemplateSpec.text(promptTemplate)
+                       promptTemplateSpec.text(userPromptTemplate)
                                .param("customerName", customerName)
                                .param("customerMessage", customerMessage))
                .call()
