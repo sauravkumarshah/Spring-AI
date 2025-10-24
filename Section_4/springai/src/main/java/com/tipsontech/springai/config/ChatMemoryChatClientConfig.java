e4b6e82db5bc176b91cd3ef3b8ef9301ed122f72
Saurav Kumar <shah.kumar.saurav@gmail.com>
2025-10-24 22:02:30 +0530

    in memory chatMemory with default conversationId

--- Section_4/springai/src/main/java/com/tipsontech/springai/config/ChatMemoryChatClientConfig.java
+++ Section_4/springai/src/main/java/com/tipsontech/springai/config/ChatMemoryChatClientConfig.java
@@ -1,6 +1,10 @@
 package com.tipsontech.springai.config;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
+import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
+import org.springframework.ai.chat.client.advisor.api.Advisor;
+import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -8,8 +12,11 @@ import org.springframework.context.annotation.Configuration;
 public class ChatMemoryChatClientConfig {
 
     @Bean("chatMemoryChatClient")
-    public ChatClient chatClient(ChatClient.Builder chatClientBuilder) {
+    public ChatClient chatClient(ChatClient.Builder chatClientBuilder, ChatMemory chatMemory) {
+        Advisor loggerAdvisor = new SimpleLoggerAdvisor();
+        Advisor memoryAdvisor = MessageChatMemoryAdvisor.builder(chatMemory).build();
         return chatClientBuilder
+                .defaultAdvisors(loggerAdvisor, memoryAdvisor)
                 .build();
     }
 }
